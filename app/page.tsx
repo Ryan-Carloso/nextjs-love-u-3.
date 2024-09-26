61c94ba8077e58bbdac09e3882e047482918cb9a
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2024-09-26 22:12:06 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -86,7 +86,7 @@ export default function DatePickerWithSupabase() {
                       onChange={handleDateChange}
                       dateFormat="yyyy-MM-dd"
                       placeholderText="Select a date"
-                      className="block w-full px-3 py-2 sm:text-sm border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                      className="block w-full mr-10 px-3 py-2 sm:text-sm border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
                       ref={datePickerRef}
                     />
                     <button
